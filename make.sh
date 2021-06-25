a87801ec83efc0e5031874908deec0ccc1c543e0
doctor-design <71015256+doctor-design@users.noreply.github.com>
2021-06-26 07:50:45 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -120,7 +120,6 @@ EOF
 
 get_kernel(){
 	KERNEL_FOLDER_NAME=$(echo ${KERNEL_VERSION/+o/-o})
-	echo ${KERNEL_FOLDER_NAME}
 	svn co ${KERNEL_URL}/${KERNEL_FOLDER_NAME}/kernel  >/dev/null 2>&1
 	cp -r kernel/* /opt/kernel
 }
