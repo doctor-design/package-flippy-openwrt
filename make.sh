100367f17258b3bb716859e0beeb7261877085f6
doctor-design <71015256+doctor-design@users.noreply.github.com>
2021-07-13 14:29:42 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -123,6 +123,8 @@ EOF
 get_kernel(){
         KERNEL_FOLDER_NAME=$(echo ${KERNEL_VERSION/+o/-o})
         KERNEL_FOLDER_NAME=$(echo ${KERNEL_FOLDER_NAME} | sed 's/+//g')
+        ls
+	rm -rf  kernel/*
 	echo ${KERNEL_FOLDER_NAME}  #显示内核路径
         svn co ${KERNEL_URL}/${KERNEL_FOLDER_NAME}/kernel  # >/dev/null 2>&1
         cp -r kernel/* /opt/kernel
